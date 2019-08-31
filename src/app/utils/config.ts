466bcd22ce07c6310e68d4c3a0cb5106119a80cf
sharkee89 <stefannikolic989@gmail.com>
2019-08-31 17:56:46 +0200

    Adding local test url

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -1,1 +1,1 @@
-export const TODO_SERVICE_URL = 'https://young-dawn-58008.herokuapp.com';
+export const TODO_SERVICE_URL = 'https://young-dawn-58008.herokuapp.com'; // http://localhost:3000/ - for local demo
